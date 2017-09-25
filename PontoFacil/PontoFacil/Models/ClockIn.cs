706b7f0ad3b834d839407063a6cc7e4492416b64
Thiago Rocha <tpr@itsproject.cin.ufpe.br>
2017-09-25 16:22:30 -0300

    ClockIn Service

--- PontoFacil/PontoFacil/Models/ClockIn.cs
+++ PontoFacil/PontoFacil/Models/ClockIn.cs
@@ -1,12 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PontoFacil.Models
 {
     class ClockIn
     {
+        private bool _isOpen;
+        public bool IsOpen
+        {
+            get { return _isOpen; }
+            private set { _isOpen = value; }
+        }
+
+        private DateTime _start;
+        private DateTime Start
+        {
+            get { return _start; }
+            set { _start = value; }
+        }
+
+        private DateTime _end;
+        private DateTime End
+        {
+            get { return _end; }
+            set { _end = value; }
+        }
+
+        public void Open(DateTime dt)
+        {
+            _start = dt;
+            _isOpen = false;
+        }
+
+        public void Close(DateTime dt)
+        {
+            _end = dt;
+            _isOpen = false;
+        }
+
+        public ClockIn()
+        {
+            _isOpen = true;
+        }
     }
 }
--- PontoFacil/PontoFacil/Services/ClockInService.cs
+++ PontoFacil/PontoFacil/Services/ClockInService.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PontoFacil.Models;
+using PontoFacil.Repositories;
+using System;
 
 namespace PontoFacil.Services
 {
     class ClockInService
     {
+        private IRepository<ClockIn> Rep;
+        private ClockIn clockIn;
+
+        public ClockInService(IRepository<ClockIn> rep)
+        {
+            clockIn = new ClockIn();
+            Rep = rep;
+        }
+
+        public void Register(DateTime date)
+        {
+            if (clockIn.IsOpen)
+                EndCurrentDay(date);
+            else
+                StartNewDay(date);
+        }
+
+        private void StartNewDay(DateTime dt)
+        {
+            clockIn = new ClockIn();
+            clockIn.Open(dt);
+
+            //Rep add new day
+        }
+
+        private void EndCurrentDay(DateTime dt)
+        {
+            clockIn.Close(dt);
+            Rep.SaveClockIn(clockIn);
+        }
     }
 }
