37f49effab71d94331fe710b1ca31dcedfefabca
ITSPROJECT\tpr <tprcin@gmail.com>
2017-09-27 11:11:09 -0300

    Creating Profile class

--- PontoFacil/PontoFacil/Models/Profile.cs
+++ PontoFacil/PontoFacil/Models/Profile.cs
@@ -8,5 +8,45 @@ namespace PontoFacil.Models
 {
     class Profile
     {
+        #region Properties
+        private string name;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { name = value; }
+        }
+
+        private DateTime defaultBegin;
+
+        public DateTime DefaultBegin
+        {
+            get { return this.defaultBegin; }
+            set { defaultBegin = value; }
+        }
+
+        private DateTime lunchPeriod;
+
+        public DateTime LunchPeriod {
+            get { return lunchPeriod; }
+            set { lunchPeriod = value;  }
+        }
+        
+        private DateTime defaultFinish;
+
+        public DateTime DefaultFinish
+        {
+            get { return this.defaultFinish; }
+            set { defaultFinish = value; }
+        }
+        
+        private DateTime accumulatedHours;
+
+        public DateTime AccumulatedHours
+        {
+            get { return this.accumulatedHours; }
+            set { accumulatedHours = value; }
+        }
+        #endregion
     }
 }
