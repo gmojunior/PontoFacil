67a0c4c3c8ee2e2ee603de22cd515c57fdbd1bb3
Thiago Pereira Rocha <tpr@itsproject.cin.ufpe.br>
2017-09-27 14:42:24 -0300

    Source Format

--- PontoFacil/PontoFacil/Views/MainPage.xaml.cs
+++ PontoFacil/PontoFacil/Views/MainPage.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -34,7 +23,7 @@ namespace PontoFacil.Views
 
         private void MenuButtonHome_Click(object sender, RoutedEventArgs e)
         {
-            //FrameContent.Navigate(typeof());
+            throw new NotImplementedException();
         }
 
         private void MenuButtonPlanning_Click(object sender, RoutedEventArgs e)
